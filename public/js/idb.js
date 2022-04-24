53260ec3df597a647ac86a52fed02776e1aa5047
escowin <edwin@escowinart.com>
2022-04-23 22:52:05 -0500

    idb.js | requests & functions

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,36 +1,67 @@
-// create a variable to hold db connection 
 let db;
-//establishing a conne connection to IndexDb database and set the version of db to 1
-const request = indexedDB.open('budegt-tracker', 1);
-// add an event to emit if the db version change 
-request.onupgradeneeded = function(event) {
-    //save a refence to the database 
-    const db = event.target.result;
-    // an object to store (table) called new_budget, with out increment PK
-    db.createObjectStore('new_budget', {autoIncrement: true});
-}
-    // upon a success request 
-    request.onsuccess = function(event) {
-        // if the db successfully created with its object store from onupgradeneeded, save the refernce to db global variable 
-        db = event.target.result;
-        // check if app is online 
-        // if the app is online run sendTransaction()
-        if(navigatore.online) {
-            //sendTransaction()
 
-        }
-    }; 
-    // if there is an error 
-    request.onerror = function(event) {
-        console.log(event.target.errorCode);
+const request = indexedDB.open('budget-tracker', 1);
+
+// request | ties upgrade to database 
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+// request | succesful requests save references to global db (js:1)
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.online) {
+        uploadBudget();
     }
-// save records 
-//a function will be executed if we attempt to submit a new budget and there's no internet connection
+};
+
+// request | error c.log(...error code)
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+
+// function | save record
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions
     const transaction = db.transaction(['new_budget'], 'readwrite');
-     // access the object store for `new_budget`
-     const newObjectStore = transaction.objectStore('new_budget');
-    // add record to your store with add method
+    const newObjectStore = transaction.objectStore('new_budget');
     newObjectStore.add(record);
-}
+};
+
+// function | upload transaction
+function uploadTransaction() {
+    const transaction = db.transaction(["new_transaction"], "readwrite");
+    const transactionObjectStore = transaction.objectStore("new_transaction");
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json",
+            },
+          })
+            .then((response) => response.json())
+            .then((serverResponse) => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+
+              const transaction = db.transaction(["new_transaction"], "readwrite");
+              const transactionObjectStore = transaction.objectStore("new_transaction");
+              transactionObjectStore.clear();
+
+              alert("all transactions submitted");
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      };
+    }
+
+window.addEventListener('online', uploadTransaction); 
