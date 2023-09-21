45707ad88c61453157822d9bdd943f59d6ff5552
escowin <edwin@escowinart.com>
2023-09-21 14:18:24 -0500

    built up timestamp format test accuracy with regex match

--- server/__tests__/helpers.test.js
+++ server/__tests__/helpers.test.js
@@ -7,7 +7,10 @@ test("test validation", () => {
     expect(validate.type(value.b, arr)).toBe(false)
 })
 
-test("returns YYYY-MM-DD hh:mm a timestamp", () => {
+test("returns YYYY-MM-DD hh:mm:ss a timestamp", () => {
     const date = new Date()
-    expect(format.date(date)).toEqual(expect.any(String))
+    const formattedDate = format.date(date)
+    const regex = /^\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}:\d{2} [APap][Mm]$/;
+
+    expect(regex.test(formattedDate)).toBe(true)
 })
