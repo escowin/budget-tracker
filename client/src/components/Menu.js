f45d534da3928c5d2bdec951091f973b07404999
escowin <edwin@escowinart.com>
2023-09-28 16:05:35 -0500

    logged button onClick case returns, wrote functional back button

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom";
-import Auth from "../utils/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation, useQuery } from "@apollo/client";
+import { DELETE_BUDGET, DELETE_ITEM } from "../utils/mutations";
+import { QUERY_SELF } from "../utils/queries";
 import { format } from "../utils/helpers";
+import Auth from "../utils/auth";
 
 function Menu({ menu, el, ulClass }) {
+  const navigate = useNavigate();
+  // const [remove] = useMutation(DELETE_BUDGET, {
+  //   update(cache, { data }) {
+  //     const { self } = cache.readQuery({ query: QUERY_SELF });
+  //   },
+  // });
   const Element = el === "link" ? Link : el;
 
   const logout = (e) => {
@@ -15,7 +24,23 @@ function Menu({ menu, el, ulClass }) {
   };
 
   const btnAttributes = (option) => {
-    return { onClick: () => console.log(option) };
+    return { onClick: () => handleChange(option) };
+  };
+
+  const handleChange = (option) => {
+    switch (option) {
+      case "delete":
+        console.log(option);
+        break;
+      case "edit":
+        console.log(option);
+        break;
+      case "back":
+        navigate(-1);
+        break;
+      default:
+        console.log("invalid case");
+    }
   };
 
   return (
